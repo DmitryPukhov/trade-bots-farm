6b170c983b0c8f18badb582266bd672c72a1288a
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-10 09:17:18 +0300

    connector_stream_htx updated to async sockets

--- connectors/stream/htx-ws/src/connector_stream_htx_app.py
+++ connectors/stream/htx-ws/src/connector_stream_htx_app.py
@@ -1,13 +1,10 @@
+import asyncio
 import logging
 import os
-import threading
-
-from prometheus_client import start_http_server
 
 from common_tools import CommonTools
-from htx_websocket_client import HtxWebSocketClient
+from htx_web_socket_client import HtxWebSocketClient
 from kafka_raw_producer import KafkaRawProducer
-from metrics import Metrics
 
 
 class ConnectorStreamHtxApp:
@@ -16,47 +13,43 @@ class ConnectorStreamHtxApp:
     def __init__(self):
 
         CommonTools.init_logging()
-        # Create websocket client, don't run just now
-        self.__websocket_client_market = self.create_websocket_client()
 
-
-    def create_websocket_client(self):
-        # Get key, secret
-        htx_access_key = os.environ.get('HTX_ACCESS_KEY')
-        htx_secret_key = os.environ.get('HTX_SECRET_KEY')
-        if not htx_access_key or not htx_secret_key:
+        # Get connection requisites
+        self.htx_access_key = os.environ.get('HTX_ACCESS_KEY')
+        self.htx_secret_key = os.environ.get('HTX_SECRET_KEY')
+        self.htx_host = os.environ.get('HTX_HOST') or 'api.hbdm.com'
+        self.htx_path = os.environ.get('HTX_PATH') or '/linear-swap-ws'
+        if not self.htx_access_key or not self.htx_secret_key:
             raise Exception("HTX_ACCESS_KEY or HTX_SECRET_KEY not set")
 
         # Get topics to listen
-        topics = [topic.strip() for topic in os.environ['HTX_TOPICS'].split(',')]
-        if not topics:
+        self.topics = [topic.strip() for topic in os.environ['HTX_TOPICS'].split(',')]
+        if not self.topics:
             raise Exception("HTX_TOPICS not set")
-        logging.info("HTX_TOPICS: %s", topics)
+        logging.info("HTX_TOPICS: %s", self.topics)
 
-        # Get connection requisites
-        htx_host = os.environ.get('HTX_HOST') or 'api.hbdm.com'
-        htx_path = os.environ.get('HTX_PATH') or '/linear-swap-ws'
+    async def run_async(self):
+        """ Listen to HTX messages, produce them to Kafka"""
 
+        # Set up the Kafka producer and the HTX client to run together
         kafka_raw_producer = KafkaRawProducer()
+        client =  HtxWebSocketClient(topics=self.topics,
+                                     host=self.htx_host,
+                                     path=self.htx_path,
+                                     access_key=self.htx_access_key,
+                                     secret_key=self.htx_secret_key,
+                                     be_spot=False,
+                                     is_broker=False,
+                                     receiver=kafka_raw_producer)
 
-        # Create websocket client, don't run just now
-        return HtxWebSocketClient(topics=topics,
-                                                            host=htx_host,
-                                                            path=htx_path,
-                                                            access_key=htx_access_key,
-                                                            secret_key=htx_secret_key,
-                                                            be_spot=False,
-                                                            is_broker=False,
-                                                            receiver=kafka_raw_producer)
+        # Connect to HTX and listen to the messages
+        try:
+            await client.connect()
+        except asyncio.CancelledError:
+            await client.close()
 
     def run(self):
-        # Start Prometheus HTTP server for metrics (runs on port 8000 by default)
-        #start_http_server(self._metrics_port)
-        threading.Thread(target=Metrics.push_to_gateway_periodical).start()
-        kafka_raw_producer = KafkaRawProducer()
-        websocket_client = HtxWebSocketClient(receiver=kafka_raw_producer)
-        websocket_client.open()
-
+        asyncio.run(self.run_async())
 
 if __name__ == '__main__':
     ConnectorStreamHtxApp().run()
