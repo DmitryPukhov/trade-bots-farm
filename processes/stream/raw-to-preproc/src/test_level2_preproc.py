0224a2e72a716e8f3c6ff8a61b27cd47c3ae7a7e
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-13 19:16:39 +0300

    test_level2_preproc.py fixed

--- processes/stream/raw-to-preproc/src/test_level2_preproc.py
+++ processes/stream/raw-to-preproc/src/test_level2_preproc.py
@@ -1,18 +1,19 @@
 import json
-from unittest import TestCase
-
+import pytest
 import pandas as pd
 
 from level2_preproc import Level2Preproc
 
 
-class TestLevel2Preproc(TestCase):
-    def test_process(self):
+class TestLevel2Preproc:
+    @pytest.mark.asyncio
+    async def test_process(self):
         ts1 = pd.Timestamp("2025-06-03 14:11:00")
         ts2 = pd.Timestamp("2025-06-03 14:12:10")
         ts3 = pd.Timestamp("2025-06-03 14:12:11")
 
         msg = {
+            "ch": "topic1",
             "tick": {
                 "bids": [[98, 1], [99, 2]],
                 "asks": [[101, 3], [102, 4]],
@@ -24,52 +25,55 @@ class TestLevel2Preproc(TestCase):
 
         # Accumulate minute 1, don't process
         msg["tick"]["ts"] = ts1.value // 1_000_000
-        preprocessed = pd.DataFrame(level2_preproc.process(json.dumps(msg)))
-        self.assertTrue(preprocessed.empty)
-        self.assertListEqual([pd.Timestamp("2025-06-03 14:11:00")], list(level2_preproc._buffer.keys()))
+        preprocessed = pd.DataFrame(await level2_preproc.process(json.dumps(msg)))
+        assert preprocessed.empty
+        assert list(level2_preproc._buffer.keys()) == [pd.Timestamp("2025-06-03 14:11:00")]
 
         # Accumulate minute 2, don't process minute 1 because of timeout not elapsed
         msg["tick"]["ts"] = ts2.value // 1_000_000
-        preprocessed = pd.DataFrame(level2_preproc.process(json.dumps(msg)))
-        self.assertTrue(preprocessed.empty)
-        self.assertListEqual([pd.Timestamp("2025-06-03 14:11:00"), pd.Timestamp("2025-06-03 14:12:00")], list(level2_preproc._buffer.keys()))
+        preprocessed = pd.DataFrame(await level2_preproc.process(json.dumps(msg)))
+        assert preprocessed.empty
+        assert list(level2_preproc._buffer.keys()) == [pd.Timestamp("2025-06-03 14:11:00"), pd.Timestamp("2025-06-03 14:12:00")]
 
         # Accumulate minute 2, process minute 1 and delete from buffer
         msg["tick"]["ts"] = ts3.value // 1_000_000
-        preprocessed = pd.DataFrame(level2_preproc.process(json.dumps(msg)))
-        self.assertEqual(1, len(preprocessed))
-        self.assertListEqual([pd.Timestamp("2025-06-03 14:12:00")], list(level2_preproc._buffer.keys()))
+        preprocessed = pd.DataFrame(await level2_preproc.process(json.dumps(msg)))
+        assert len(preprocessed) == 1
+        assert list(level2_preproc._buffer.keys()) == [pd.Timestamp("2025-06-03 14:12:00")]
 
-    def test_transform_message(self):
+    @pytest.mark.asyncio
+    async def test_transform_message(self):
         raw_msg = {
+            "ch": "topic1",
             "tick": {
                 "ts": pd.Timestamp("2025-06-03 14:11:00").value / 1_000_000,
                 "bids": [[101, 1], [102, 2]],
                 "asks": [[103, 3], [104, 4]]
             }
         }
-        transformed = Level2Preproc()._transform_message(raw_msg)
+        transformed = await Level2Preproc()._transform_message(raw_msg)
 
-        self.assertEqual(transformed.bid_max, 102)
-        self.assertEqual(transformed.ask_min, 103)
-        self.assertEqual(transformed.bid_vol_sum, 3)
-        self.assertEqual(transformed.ask_vol_sum, 7)
-        self.assertEqual(transformed.bid_mul_vol_sum, 101 * 1 + 102 * 2)
-        self.assertEqual(transformed.ask_mul_vol_sum, 103 * 3 + 104 * 4)
-        self.assertEqual(transformed.bid_expect, (101 * 1 + 102 * 2) / (1 + 2))
-        self.assertEqual(transformed.ask_expect, (103 * 3 + 104 * 4) / (3 + 4))
-        self.assertEqual(transformed.expect,
-                         ((103 * 3 + 104 * 4) - (101 * 1 + 102 * 2)) / (1 + 2 + 3 + 4))
+        assert transformed.bid_max == 102
+        assert transformed.ask_min == 103
+        assert transformed.bid_vol_sum == 3
+        assert transformed.ask_vol_sum == 7
+        assert transformed.bid_mul_vol_sum == 101 * 1 + 102 * 2
+        assert transformed.ask_mul_vol_sum == 103 * 3 + 104 * 4
+        assert transformed.bid_expect == (101 * 1 + 102 * 2) / (1 + 2)
+        assert transformed.ask_expect == (103 * 3 + 104 * 4) / (3 + 4)
+        assert transformed.expect ==((103 * 3 + 104 * 4) - (101 * 1 + 102 * 2)) / (1 + 2 + 3 + 4)
 
-    def test_aggregate(self):
+    @pytest.mark.asyncio
+    async def test_aggregate(self):
         raw_msg = {
+            "ch": "topic1",
             "tick": {
                 "ts":pd.Timestamp("2025-06-03 14:11:00").value/1_000_000,
                 "bids": [[101, 1], [102, 2]],
                 "asks": [[103, 3], [104, 4]]
             }
         }
-        aggregated = Level2Preproc()._aggregate([raw_msg])
+        aggregated = await Level2Preproc()._aggregate([raw_msg])
 
         # Just ensure something is calculated, no need to test pandas mean function
-        self.assertEqual(aggregated["bid_max"], 102)
+        assert aggregated["bid_max"] == 102
