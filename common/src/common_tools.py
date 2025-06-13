78676538ae5c97606aa125f952634fc5a6f8b231
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-13 16:15:08 +0300

    Fixed empty metrics

--- common/src/common_tools.py
+++ common/src/common_tools.py
@@ -11,3 +11,7 @@ class CommonTools:
             level=os.environ.get("LOG_LEVEL") or logging.INFO,
             format='%(asctime)s -  %(module)s.%(funcName)s:%(lineno)d  - %(levelname)s - %(message)s'
         )
+        logging.error("Error test")
+        logging.warning("Warning test")
+        logging.info("Info test")
+        logging.debug("Debug test")
