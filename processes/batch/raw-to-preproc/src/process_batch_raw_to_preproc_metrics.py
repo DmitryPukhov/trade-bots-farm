a7763f23c4b61a54d288d991d8ddeec4abb9163d
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-13 17:40:27 +0300

    batch preprocessing metrics added

---
+++ processes/batch/raw-to-preproc/src/process_batch_raw_to_preproc_metrics.py
@@ -0,0 +1,23 @@
+from prometheus_client import Counter
+
+from metrics_base import MetricsBase
+
+
+class ProcessBatchRawToPreprocMetrics(MetricsBase):
+    namespace = "process_batch_raw_to_preproc"
+
+    files_transferred = Counter(
+        '_files_transferred',
+        'Total number of files downloaded',
+        ['preproc_s3_dir'],
+        namespace = namespace,
+        registry = MetricsBase._registry
+    )
+
+
+    job_runs = Counter(
+        '_job_runs',
+        'Total runs of this job',
+        namespace = namespace,
+        registry = MetricsBase._registry
+    )
