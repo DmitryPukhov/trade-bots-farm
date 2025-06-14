b740cc6a05434b995943ffec7f0bdaf0f17b5b52
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-14 19:38:59 +0300

    Initial stream features

---
+++ processes/stream/features/src/features_metrics.py
@@ -0,0 +1,62 @@
+from prometheus_client import Counter, Gauge
+
+from metrics_base import MetricsBase
+
+
+class FeaturesMetrics(MetricsBase):
+    namespace = "process_stream_features"
+    # Kafka and s3 input messages
+    # Kakfa and s3 input time lag
+    input_kafka_messages = Counter(
+        '_kafka_input_messages',
+        'Total number of input Kafka messages',
+        ['topic'],
+        namespace=namespace,
+        registry=MetricsBase._registry
+    )
+
+    input_s3_rows = Counter(
+        '_input_s3_rows',
+        'Rows in s3 input dataset',
+        ['s3_dir'],
+        namespace=namespace,
+        registry=MetricsBase._registry
+    )
+
+    input_s3_rows_good = Counter(
+        '_input_s3_rows_good',
+        'Rows in s3 input dataset',
+        ['feature_name'],
+        namespace=namespace,
+        registry=MetricsBase._registry
+    )
+    input_s3_rows_not_merged = Counter(
+        '_input_s3_rows_not_merged',
+        'Rows in s3 input dataset not merged due to na after merge level2 and candles',
+        ['feature_name'],
+        namespace=namespace,
+        registry=MetricsBase._registry
+    )
+
+    input_s3_time_lag_sec = Gauge(
+        '_input_s3_time_lag_sec',
+        'Lag between last s3 data and current time',
+        ['s3_dir'],
+        namespace=namespace,
+        registry=MetricsBase._registry
+    )
+
+    feature_time_lag_sec = Gauge(
+        '_feature_time_lag_sec',
+        'Lag between feature timestamp and current time',
+        ['topic'],
+        namespace=namespace,
+        registry=MetricsBase._registry
+    )
+    output_messages = Counter(
+        '_feature_output_messages',
+        'Total number of output features',
+        ['topic'],
+        namespace=namespace,
+        registry=MetricsBase._registry
+    )
---
+++ processes/stream/features/src/multi_indi_features_dag.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.empty import EmptyOperator
+
+from dag_tools import tbf_task_operator
+
+# Default arguments for the DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+}
+
+# Define the DAG
+with DAG(
+        'process_stream_features_milti_indi',
+        default_args=default_args,
+        schedule_interval=None,
+        start_date=datetime(2023, 1, 1),
+        catchup=False,
+        tags=['trade-bots-farm'],
+        max_active_runs=1
+) as dag:
+    # create [(source, dest, kind)] from config
+
+    task_envs = [
+        # Process level2
+        {"KAFKA_TOPIC_SRC": "raw.htx.market.btc-usdt.depth.step13", "KAFKA_TOPIC_DST": "preproc.btc-usdt.level2.1min",
+         "KIND": "level2", "TICKER": "btc-usdt"},
+        # Process candles
+        {"KAFKA_TOPIC_SRC": "raw.htx.market.btc-usdt.kline.1min",
+         "KAFKA_TOPIC_DST": "preproc.btc-usdt.candles.1min",
+         "KIND": "candles", "TICKER": "btc-usdt"}
+    ]
+
+    # Create tasks list for each source,  dest, kind
+    parallel_tasks = []
+    for task_env in task_envs:
+        ticker = "btc-usdt"
+        task_id = f"process_stream_features_multi_indi_{ticker}"
+        # Parallel process level2, candles, bid/ask if configured
+        task_operator = tbf_task_operator(
+            task_id=task_id,
+            wheel_file_name="trade_bots_farm_process_stream_features-0.1.0-py3-none-any.whl",
+            module_name="multi_indi_features_app",
+            class_name="MultiIndiFeaturesApp",
+            **{"env_vars": task_env})
+        parallel_tasks.append(task_operator)
+
+    # Final workflow
+    EmptyOperator(task_id="start") >> parallel_tasks
