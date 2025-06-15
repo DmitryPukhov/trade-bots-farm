79a78e3eaa7a2454d6562e8b9d9d8cec74112b6e
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-15 13:22:59 +0300

    Setting log level: prints to console

--- common/src/common_tools.py
+++ common/src/common_tools.py
@@ -6,9 +6,11 @@ class CommonTools:
 
     @staticmethod
     def init_logging():
+        log_level = os.environ.get("LOG_LEVEL") or logging.INFO
+        print(f"Init logging, set log level to {log_level}")
         # Setup logging
         logging.basicConfig(
-            level=os.environ.get("LOG_LEVEL") or logging.INFO,
+            level=log_level,
             format='%(asctime)s -  %(module)s.%(funcName)s:%(lineno)d  - %(levelname)s - %(message)s'
         )
         logging.error("Error test")
