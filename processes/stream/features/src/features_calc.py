317d145509c3ffbef07b0ed39b46590bdddc4d18
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-16 16:41:20 +0300

    stream features unit tests are fixed.

--- processes/stream/features/src/features_calc.py
+++ processes/stream/features/src/features_calc.py
@@ -12,11 +12,12 @@ class FeaturesCalc:
         self._stop_event = stop_event
 
     async def calc(self, df: pd.DataFrame):
-        logging.info(f"Calculating features. Input shape: {df.shape}")
+        logging.info(f"Calculating features. Last time: {df.index.max()}")
         return pd.DataFrame()
 
     async def run_async(self):
         while not self._stop_event.is_set():
             await self._feed.new_data_event.wait()
+            self._feed.new_data_event.clear()
             df = self._feed.data.copy()
             features = await self.calc(df)
