1b4452241da8a43be894f5a74289066c27b84156
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-19 10:52:02 +0300

    stream level2 created conversion from htx raw to pytrade2 raw

--- processes/stream/raw-to-preproc/src/test_level2_preproc.py
+++ processes/stream/raw-to-preproc/src/test_level2_preproc.py
@@ -1,6 +1,9 @@
 import json
-import pytest
+
+import numpy as np
 import pandas as pd
+import pytest
+from pandas.testing import assert_series_equal
 
 from level2_preproc import Level2Preproc
 
@@ -33,7 +36,8 @@ class TestLevel2Preproc:
         msg["tick"]["ts"] = ts2.value // 1_000_000
         preprocessed = pd.DataFrame(await level2_preproc.process(json.dumps(msg)))
         assert preprocessed.empty
-        assert list(level2_preproc._buffer.keys()) == [pd.Timestamp("2025-06-03 14:11:00"), pd.Timestamp("2025-06-03 14:12:00")]
+        assert list(level2_preproc._buffer.keys()) == [pd.Timestamp("2025-06-03 14:11:00"),
+                                                       pd.Timestamp("2025-06-03 14:12:00")]
 
         # Accumulate minute 2, process minute 1 and delete from buffer
         msg["tick"]["ts"] = ts3.value // 1_000_000
@@ -68,7 +72,7 @@ class TestLevel2Preproc:
         raw_msg = {
             "ch": "topic1",
             "tick": {
-                "ts":pd.Timestamp("2025-06-03 14:11:00").value/1_000_000,
+                "ts": pd.Timestamp("2025-06-03 14:11:00").value / 1_000_000,
                 "bids": [[101, 1], [102, 2]],
                 "asks": [[103, 3], [104, 4]]
             }
@@ -78,3 +82,56 @@ class TestLevel2Preproc:
         # Just ensure something is calculated, no need to test pandas mean function
         assert len(aggregated) == 1
         assert aggregated[0]["bid_max"] == 102
+
+    @pytest.mark.asyncio
+    async def test_htx_raw_to_pytrade2_raw_df_should_convert_empty_list(self):
+        converted = await Level2Preproc()._htx_raw_to_pytrade2_raw_df([])
+        assert converted is not None
+        assert converted.empty
+
+    @pytest.mark.asyncio
+    async def test_htx_raw_to_pytrade2_raw_df_should_convert_list_of_single_msg(self):
+        raw_msgs = [
+            {
+                "ch": "topic1",
+                "tick": {
+                    "ts": pd.Timestamp("2025-06-03 14:11:00").value / 1_000_000,
+                    "bids": [[101, 1], [102, 2]],
+                    "asks": [[103, 3], [104, 4]]
+                }
+            },
+            {
+                "ch": "topic1",
+                "tick": {
+                    "ts": pd.Timestamp("2025-06-03 14:12:00").value / 1_000_000,
+                    "bids": [[201, 21], [202, 22]],
+                    "asks": [[203, 23], [204, 24]]
+                }
+            },
+
+        ]
+
+        # Call conversion
+        converted = await Level2Preproc()._htx_raw_to_pytrade2_raw_df(raw_msgs)
+
+        # Assert that htx raw input converted to pytrade2 raw input, with a row per bid or ask
+        assert converted.index.to_list() == [pd.Timestamp("2025-06-03 14:11:00")] * 4 + [
+            pd.Timestamp("2025-06-03 14:12:00")] * 4
+        assert_series_equal(converted["datetime"],
+                            pd.Series([pd.Timestamp("2025-06-03 14:11:00")] * 4
+                                      + [pd.Timestamp("2025-06-03 14:12:00")] * 4,
+                                      name="datetime", index=converted.index)
+                            .astype("datetime64[ms]"))
+        assert_series_equal(converted["bid"],
+                            pd.Series([101.0, 102.0, np.nan, np.nan, 201.0, 202.0, np.nan, np.nan], name="bid",
+                                      index=converted.index))
+        assert_series_equal(converted["bid_vol"],
+                            pd.Series([1.0, 2.0, np.nan, np.nan, 21.0, 22.0, np.nan, np.nan], name="bid_vol",
+                                      index=converted.index))
+
+        assert_series_equal(converted["ask"],
+                            pd.Series([np.nan, np.nan, 103.0, 104.0, np.nan, np.nan, 203.0, 204.0], name="ask",
+                                      index=converted.index))
+        assert_series_equal(converted["ask_vol"],
+                            pd.Series([np.nan, np.nan, 3.0, 4.0, np.nan, np.nan, 23.0, 24.0], name="ask_vol",
+                                      index=converted.index))
