c20071fab23f73e646733891b6dd45a5873fc35c
dmitriy.pukhov <pukhov@gnivc.ru>
2025-06-24 17:52:15 +0300

    test_multi_indi_features_calc.py extended

--- processes/stream/features/src/test_multi_indi_features_calc.py
+++ processes/stream/features/src/test_multi_indi_features_calc.py
@@ -7,33 +7,30 @@ from multi_indi_features_calc import MultiIndiFeaturesCalc
 class TestMultiIndiFeaturesCalc:
 
     def params(self):
-        return {"1min": {"cca": {"window": 2},
+        period_params = {"cca": {"window": 2},
                          "ichimoku": {"window1": 1, "window2": 1, "window3": 1},
-                         "adx": {"window": 1},
+                         "adx": {"window": 2},
                          "rsi": {"window": 1},
                          "stoch": {"window": 2, "smooth_window": 1},
                          "macd": {"slow": 1, "fast": 1}
-                         }}
+                         }
+        return {
+                "5min": period_params,
+                "10min": period_params}
 
     @pytest.mark.asyncio
     async def test_calc(self):
-        times = ["2025-06-24 13:01:00", "2025-06-24 13:02:00", "2025-06-24 13:03:00","2025-06-24 13:04:00", "2025-06-24 13:05:00", "2025-06-24 13:01:00"]
-        input_df = pd.DataFrame({
-            'datetime': times,
-            "l2_bid_max": [1, 2, 3, 4, 5, 6],
-            "l2_bid_expect": [1, 2, 3, 4, 5, 6],
-            "l2_bid_vol": [1, 2, 3, 4, 5, 6],
-            "l2_ask_min": [1, 2, 3, 4, 5, 6],
-            "l2_ask_expect": [1, 2, 3, 4, 5, 6],
-            "l2_ask_vol": [1, 2, 3, 4, 5, 6],
-            "open_time": times,
-            "close_time": times,
-            "open": [1, 2, 3, 4, 5, 6],
-            "high": [1, 2, 3, 4, 5, 6],
-            "low": [1, 2, 3, 4, 5, 6],
-            "close": [1, 2, 3, 4, 5, 6],
-            "vol": [1, 2, 3, 4, 5, 6],
-        })
+        start_time = pd.Timestamp("2025-06-24 13:01:00")
+        n = 100
+        times = [start_time + pd.Timedelta(minutes=i) for i in range(n)]
+
+        input_data = {"datetime": times, "open_time": times, "close_time": times}
+
+        for col in ["l2_bid_max", "l2_bid_expect", "l2_bid_vol", "l2_ask_min", "l2_ask_expect", "l2_ask_vol",
+                    "open_time", "close_time", "open", "high", "low", "close", "vol"]:
+            input_data[col] = range(len(times))
+
+        input_df = pd.DataFrame(input_data)
         time_cols = ['datetime', 'open_time', 'close_time']
         input_df[time_cols] = input_df[time_cols].apply(pd.to_datetime)
         input_df.set_index("datetime", drop=False, inplace=True)
