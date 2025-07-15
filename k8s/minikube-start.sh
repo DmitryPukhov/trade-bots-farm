de1bc1d751c9207ebca254cd73cae199f6806b4d
dmitriy.pukhov <pukhov@gnivc.ru>
2025-07-15 19:10:58 +0300

    minikube-start.sh added to use kvm minikube driver

--- k8s/minikube-start.sh
+++ k8s/minikube-start.sh
@@ -4,6 +4,6 @@ MINIKUBE_REGISTRY_PATH="/var/lib/registry"
 echo "Starting minikube"
 echo "Mounting host $HOST_REGISTRY_PATH to minikube $MINIKUBE_REGISTRY_PATH"
 
-minikube start --driver=kvm2 --cpus=4 --memory=16384  --mount --mount-string=$"$HOST_REGISTRY_PATH:$MINIKUBE_REGISTRY_PATH"
+minikube start --driver=kvm2 --cpus=6 --memory=32g  --mount --mount-string=$"$HOST_REGISTRY_PATH:$MINIKUBE_REGISTRY_PATH"
 minikube addons enable metrics-server
 minikube addons enable registry
