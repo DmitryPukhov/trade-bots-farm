1430f8830fae13cbba21facaa1753c2b633fbe74
dmitriy.pukhov <pukhov@gnivc.ru>
2025-07-18 17:35:02 +0300

    minikube registry persistence enabled

--- k8s/minikube-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-HOST_REGISTRY_PATH="$HOME/.minikube-registry"
-MINIKUBE_REGISTRY_PATH="/var/lib/registry"
-echo "Starting minikube"
-echo "Mounting host $HOST_REGISTRY_PATH to minikube $MINIKUBE_REGISTRY_PATH"
-
-minikube start --driver=kvm2 --cpus=6 --memory=32g  --mount --mount-string=$"$HOST_REGISTRY_PATH:$MINIKUBE_REGISTRY_PATH"
-minikube addons enable metrics-server
-minikube addons enable registry
