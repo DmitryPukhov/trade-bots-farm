45fe0a8bb0c2c5dd19528adfbe9a9a25169a8ed2
dmitriy.pukhov <pukhov@gnivc.ru>
2025-09-08 18:54:47 +0300

    Initial alor staging process

---
+++ processes/batch/staging/alor/src/process_batch_staging_alor_metrics.py
@@ -0,0 +1,29 @@
+from prometheus_client import Counter
+
+from metrics_base import MetricsBase
+
+
+class ProcessBatchRawToPreprocMetrics(MetricsBase):
+    namespace = "process_batch_staging"
+
+    files_transferred = Counter(
+        '_files_transferred',
+        'Total number of files downloaded',
+        ['preproc_s3_dir'],
+        namespace = namespace,
+        registry = MetricsBase._registry
+    )
+    rows = Counter(
+        '_rows',
+        'Total rows in input datasets',
+        ['s3_dir'],
+        namespace = namespace,
+        registry = MetricsBase._registry
+    )
+
+    job_runs = Counter(
+        '_job_runs',
+        'Total runs of this job',
+        namespace = namespace,
+        registry = MetricsBase._registry
+    )
