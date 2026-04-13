54d6393caeec5ce15a76ee3e9a22ab260056416b
dmitriy.pukhov <dmitry.pukhov@gmail.com>
2026-04-13 10:58:23 +0300

    terraform apply works with secrets

--- k8s/terraform/modules/secrets/main.tf
+++ k8s/terraform/modules/secrets/main.tf
@@ -2,12 +2,14 @@ resource "kubernetes_secret" "secrets" {
   for_each = { for file in fileset("../secret", "*.yaml") : file => file }
 
   metadata {
-    name = substr(each.value, 0, length(each.value) - 5) # Remove .yaml extension
+    name      = substr(each.value, 0, length(each.value) - 5) # Remove .yaml extension
     namespace = var.namespace
   }
 
-  # Read the YAML file and extract data
+  # Correctly parse the YAML file as an object and extract .data
   data = {
-    for k, v in yamldecode(file("../secret/${each.value}"))[0].data : k => base64decode(v)
+    for k, v in yamldecode(file("../secret/${each.value}")).stringData : k => v
   }
+
+  type = yamldecode(file("../secret/${each.value}")).type
 }
