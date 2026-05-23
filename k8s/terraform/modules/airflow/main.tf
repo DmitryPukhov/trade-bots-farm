fb432d90d074aed9bc6db33bd199a47ac471ea26
dmitriy.pukhov <dmitry.pukhov@gmail.com>
2026-05-23 14:44:29 +0300

    airflow deployment changed to ingress

--- k8s/terraform/modules/airflow/main.tf
+++ k8s/terraform/modules/airflow/main.tf
@@ -2,7 +2,7 @@ resource "helm_release" "airflow" {
   name       = "airflow"
   repository = "https://airflow.apache.org"
   chart      = "airflow"
-  version    = "1.21.0"
+  version    = "1.7.0"
   namespace  = var.namespace
   values = [
     file("${path.module}/values.yaml")
