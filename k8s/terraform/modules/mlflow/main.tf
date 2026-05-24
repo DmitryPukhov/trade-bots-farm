777f1ede899aead78049f07b48bd2698ea1589bd
dmitriy.pukhov <dmitry.pukhov@gmail.com>
2026-05-24 16:06:16 +0300

    mlflow reconfigured to community-charts

--- k8s/terraform/modules/mlflow/main.tf
+++ k8s/terraform/modules/mlflow/main.tf
@@ -1,29 +1,21 @@
 resource "helm_release" "mlflow" {
   count      = var.enabled ? 1 : 0
   name       = "mlflow"
-  repository = "https://charts.bitnami.com/bitnami"
+  repository = "https://community-charts.github.io/helm-charts"
   chart      = "mlflow"
-  version    = "10.1.3"
+  version    = "1.8.1"
   namespace  = var.namespace
   values = [
     file("${path.module}/values.yaml")
   ]
 
   depends_on = [
-    module.pvc_mlflow_tracking,
     module.pvc_mlflow_postgresql
   ]
 }
 
-module "pvc_mlflow_tracking" {
-  source    = "../pvc"
-  namespace = var.namespace
-  name      = "mlflow-tracking"
-  size      = "10Gi"
-  storage_class = "standard"
-  count     = var.enabled ? 1 : 0
-}
-
+# Pre-create the PVC for PostgreSQL data to persist across chart upgrades
+# The community chart's bundled bitnami postgresql will use this via existingClaim
 module "pvc_mlflow_postgresql" {
   source    = "../pvc"
   namespace = var.namespace
