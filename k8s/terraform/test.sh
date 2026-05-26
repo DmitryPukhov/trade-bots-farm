d90a0bd5653bc4321ee0bf4af9648b442cd4d2d7
dmitriy.pukhov <dmitry.pukhov@gmail.com>
2026-05-26 20:17:32 +0300

    Unused directories removed

--- k8s/terraform/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-set -x
-helm template test seaweedfs/seaweedfs --version 4.21.0 --set filer.ingress.enabled=true --set filer.ingress.annotations.nginx\\.ingress\\.kubernetes\\.io/auth-type=basic 2>&1 | grep -A5 "annotations:"
